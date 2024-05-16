cc7b1ef19cd86fd9218c56ba3da7788634f78341
Rogermenn <166256292+Rogermenn@users.noreply.github.com>
2024-05-16 01:44:00 -1200

    Add files via upload

--- sueñazo.js
+++ sueñazo.js
@@ -6,3 +6,17 @@ function agregarAlCarrito() {
   window.scrollTo({top: 0, behavior: 'smooth'});
 
 }
+
+let i = 0;
+let txt = 'Instrumentos favoritos'; // The text
+let speed = 50; // Speed/duration of effect in milliseconds
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector('h1 span').innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.onload = typeWriter; // Call the function when the window has finished loading
